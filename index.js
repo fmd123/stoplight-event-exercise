741bc19842b2257f3e0231d60265faed0418f54a
Francesca Davis <francescadavis@Francescas-MacBook-Pro.local>
2017-08-14 00:15:21 -0600

    done

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+var stopButton = document.getElementById("stopButton");
+var stopLight = document.getElementById("stopLight");
+
+
+stopButton.addEventListener("click", function(){
+  console.log("red clicked");
+  stopLight.classList.toggle("stop");
+
+});
+var slowButton = document.getElementById("slowButton");
+var slowLight = document.getElementById("slowLight");
+
+
+slowButton.addEventListener("click", function(){
+  console.log("yellow clicked");
+  slowLight.classList.toggle("slow");
+
+});
+var goButton = document.getElementById("goButton");
+var goLight = document.getElementById("goLight");
+goButton.addEventListener("click", function(){
+  console.log("green clicked");
+  goLight.classList.toggle("slow");
+
+});
+
+// var slowButton = document.getElementById("slowButton");
+// slowButton.addEventListener("click", function(){
+//   //toggle slowLight
+//   console.log("slowbutton clicked");
+//
+// });
+// var goButton = document.getElementById("goButton");
+// goButton.addEventListener("click", function(){
+//   //toggle goLight
+//   console.log("gobutton clicked");
+// });
+//
+
+
 })();
